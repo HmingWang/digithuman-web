6394dde3c20e25a9d2c22e144836ba5b60f0a0ad
Xest <whaim1986@gmail.com>
2019-11-08 18:41:08 +0800

    do sth

--- src/app/app.globals.ts
+++ src/app/app.globals.ts
@@ -1,1 +1,31 @@
 export const BASE_URL = 'http://localhost:8080';
+
+export const FILEFEATURE_HEADER = [
+  {field: 'id', header: '文件序号'},
+  {field: 'countword', header: '字数'},
+  {field: 'countpunc', header: '标点数'},
+  {field: 'countsentence', header: '句数'},
+  {field: 'countsection', header: '平均句长'},
+  {field: 'sentencelen', header: '平均句长方差'},
+  {field: 'sectionlen', header: '平均句长标准差'},
+  {field: 'sentencelenvar', header: '段落数'},
+  {field: 'sentencelenstd', header: '平均段长'},
+  {field: 'sectionlenvar', header: '平均段长方差'},
+  {field: 'sectionlenstd', header: '平均段长标准差'},
+  {field: 'countdialog', header: '对白数'},
+  {field: 'countbook', header: '书名数'},
+  {field: 'sentencew', header: '问号句数'},
+  {field: 'sentenceg', header: '感叹号句数'},
+  {field: 'sentencep', header: '普通句句数'},
+  {field: 'sentenceq', header: '其他句数'},
+];
+
+export const FILESTORE_HEADER = [
+  {field: 'id', header: '序号'},
+  {field: 'filename', header: '文件名'},
+  {field: 'filelen', header: '长度'},
+  {field: 'filetype', header: '类型'},
+  {field: 'encoding', header: '编码'},
+  {field: 'uptime', header: '更新时间'},
+];
+
--- src/app/shared/file-list/file-list.component.ts
+++ src/app/shared/file-list/file-list.component.ts
@@ -3,6 +3,7 @@ import {FileStoreService} from '../../services/file-store.service';
 import {FileStore} from '../../models/file-store';
 import {FileFeatureService} from '../../services/file-feature.service';
 import {FileFeature} from '../../models/file-feature';
+import {FILEFEATURE_HEADER, FILESTORE_HEADER} from '../../app.globals';
 
 @Component({
   selector: 'app-file-list',
@@ -42,34 +43,9 @@ export class FileListComponent implements OnInit {
 
   ngOnInit() {
     this.listOfData = [];
-    this.cols = [
-      {field: 'id', header: '序号'},
-      {field: 'filename', header: '文件名'},
-      {field: 'filelen', header: '长度'},
-      {field: 'filetype', header: '类型'},
-      {field: 'encoding', header: '编码'},
-      {field: 'uptime', header: '更新时间'},
-    ];
+    this.cols = FILESTORE_HEADER;
 
-    this.colDetails = [
-      {field: 'id', header: '文件序号'},
-      {field: 'countword', header: '字数'},
-      {field: 'countpunc', header: '标点数'},
-      {field: 'countsentence', header: '句数'},
-      {field: 'countsection', header: '平均句长'},
-      {field: 'sentencelen', header: '平均句长方差'},
-      {field: 'sectionlen', header: '平均句长标准差'},
-      {field: 'sentencelenvar', header: '段落数'},
-      {field: 'sentencelenstd', header: '平均段长'},
-      {field: 'sectionlenvar', header: '平均段长方差'},
-      {field: 'sectionlenstd', header: '平均段长标准差'},
-      {field: 'countdialog', header: '对白数'},
-      {field: 'countbook', header: '书名数'},
-      {field: 'sentencew', header: '问号句数'},
-      {field: 'sentenceg', header: '感叹号句数'},
-      {field: 'sentencep', header: '普通句句数'},
-      {field: 'sentenceq', header: '其他句数'},
-    ];
+    this.colDetails = FILEFEATURE_HEADER;
     this.getData();
   }
 
