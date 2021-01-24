8790c01bf74a890ad70f9cf06550f26f34d4a60a
whaim <whaim1986@gmail.com>
2021-01-24 10:40:40 +0800

    sth

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -12,16 +12,16 @@ import {FileManagerComponent} from './file-manager/file-manager.component';
 import {TestPageComponent} from './test-page/test-page.component';
 import {SegmentComponent} from './segment/segment.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { FileInfoComponent } from './file-info/file-info.component';
-import { FrequencyCharComponent } from './frequency-char/frequency-char.component';
+import {FileInfoComponent} from './file-info/file-info.component';
+import {FrequencyCharComponent} from './frequency-char/frequency-char.component';
 import {NgxEchartsModule} from 'ngx-echarts';
-import { FrequencyWordComponent } from './frequency-word/frequency-word.component';
-import { AbstractComponent } from './abstract/abstract.component';
-import { KeywordsComponent } from './keywords/keywords.component';
-import { TranslateComponent } from './translate/translate.component';
-import { ClusterComponent } from './cluster/cluster.component';
-import { ChangePasswordComponent } from './change-password/change-password.component';
-import { EditUserInfoComponent } from './edit-user-info/edit-user-info.component';
+import {FrequencyWordComponent} from './frequency-word/frequency-word.component';
+import {AbstractComponent} from './abstract/abstract.component';
+import {KeywordsComponent} from './keywords/keywords.component';
+import {TranslateComponent} from './translate/translate.component';
+import {ClusterComponent} from './cluster/cluster.component';
+import {ChangePasswordComponent} from './change-password/change-password.component';
+import {EditUserInfoComponent} from './edit-user-info/edit-user-info.component';
 
 
 @NgModule({
@@ -42,16 +42,16 @@ import { EditUserInfoComponent } from './edit-user-info/edit-user-info.component
     ClusterComponent,
     ChangePasswordComponent,
     EditUserInfoComponent,
-    ],
-    imports: [
-        CommonModule,
-        PagesRoutingModule,
-        SharedModule,
-        ServicesModule,
-        FormsModule,
-        NgxEchartsModule,
-        ReactiveFormsModule,
-    ]
+  ],
+  imports: [
+    CommonModule,
+    PagesRoutingModule,
+    SharedModule,
+    ServicesModule,
+    FormsModule,
+    NgxEchartsModule,
+    ReactiveFormsModule,
+  ]
 })
 export class PagesModule {
 }
